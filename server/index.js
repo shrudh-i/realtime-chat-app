cbb67ec669a123dc04bd0c35e728d554c880384f
shrudh-i <shrudhi.21@gmail.com>
2021-11-28 15:14:56 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = process.env.PORT  || 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`Running on Port: ${port}`);
 });
